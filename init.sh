2e27f6d8c6e8697d26bd01b902f4c4d8a46a9d97
Yi Wang <e@yi-wang.me>
2017-03-03 21:49:24 -0500

    mr location update

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-git clone https://github.com/joeyh/myrepos.git $HOME/.homesick/repos/myrepos
+git clone https://git.joeyh.name/git/mr.git/ $HOME/.homesick/repos/myrepos
 git clone git@github.com:yiwang/dotfiles.git $HOME/.homesick/repos/dotfiles
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 homeshick link
